73f5d5e828aad71375085e7599d93971a6e6d58f
Sheik Abdulla. A <sheikabdulla2411@gmail.com>
2026-04-09 08:29:08 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL:https://urban-village-backend.onrender.com/api
 });
 
 api.interceptors.request.use((config) => {
